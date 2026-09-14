cc78db204accc763564ce5fb2f771bc644a91c41
Anjali Ramesh Sangannavar <sangannavaranjali@gmail.com>
2026-09-14 15:17:12 +0530

    Add the arithmetic.js file to 06_JavaScript folder which has the practice work on arthmetic function used in javascript

---
+++ 06_JavaScript/05_arithmetic.js
@@ -0,0 +1,36 @@
+// ========================================
+// JavaScript Basics - Arithmetic
+// Program 5
+// ========================================
+
+
+// ---------- Basic Arithmetic ----------
+
+let number1 = 2;
+let number2 = 10;
+
+console.log(`The sum of the numbers is: ${number1 + number2}`);
+
+console.log(`The subtraction of the numbers is: ${number1 - number2}`);
+
+console.log(`The division of the numbers is: ${number1 / number2}`);
+
+console.log(`The multiplication of the numbers is: ${number1 * number2}`);
+
+
+// ---------- Reassigning Numbers ----------
+
+number1 = 5;
+number2 = 20;
+
+
+// ---------- Remainder ----------
+
+console.log(`The reminder of number is :${number1 % number2}`);
+
+
+// ---------- Power ----------
+
+console.log(`The power of number is :${number1 ** 2}`);
+
+console.log(`The power of number is :${number2 ** 2}`);
