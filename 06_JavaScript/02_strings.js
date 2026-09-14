bed6a83f19aa56b5995160cdbaf6414c315c4448
Anjali Ramesh Sangannavar <sangannavaranjali@gmail.com>
2026-09-14 15:13:53 +0530

    Add the strings.js file to the 06_JavaScript folder which have the practice work on string

---
+++ 06_JavaScript/02_strings.js
@@ -0,0 +1,35 @@
+// ========================================
+// JavaScript Basics - Strings
+// Program 2
+// ========================================
+
+
+// ---------- Creating Strings ----------
+
+let name1 = "Anjali no 1";
+
+let name2 = 'Anjali no 2';
+
+let name3 = `Anjali no 3`;
+
+console.log(name1);
+console.log(name2);
+console.log(name3);
+
+
+// ---------- String Concatenation ----------
+
+const firstName = "Anjali";
+
+const lastName = "Sangannavar";
+
+console.log(firstName + " " + lastName);
+
+
+// ---------- Template Literals ----------
+
+let age = 19;
+
+let city = "Hubli";
+
+console.log(`My name is ${firstName} ${lastName} I am ${age} years old. And I am from ${city}`);
