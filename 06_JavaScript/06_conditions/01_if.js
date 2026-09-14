2e99d1f5467205ba5bffbfd001521903a3b6d370
Anjali Ramesh Sangannavar <sangannavaranjali@gmail.com>
2026-09-14 15:18:39 +0530

    Create a new folder called 06_conditions in  06_JavaScript folder as a sub to add the condition related practice work

---
+++ 06_JavaScript/06_conditions/01_if.js
@@ -0,0 +1,15 @@
+// ========================================
+// JavaScript Conditions - If Statement
+// Program 1
+// ========================================
+
+
+// ---------- If Statement ----------
+
+let age = 20;
+
+if (age >= 18) {
+
+    console.log("Aault");
+
+}
