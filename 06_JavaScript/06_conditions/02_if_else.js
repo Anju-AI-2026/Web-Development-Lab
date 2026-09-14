b43368b1df69a2cc9766666e84a767e02a3d1513
Anjali Ramesh Sangannavar <sangannavaranjali@gmail.com>
2026-09-14 15:19:34 +0530

    Add the 02_if_else.js file which has practiced work on if and else statement

---
+++ 06_JavaScript/06_conditions/02_if_else.js
@@ -0,0 +1,19 @@
+// ========================================
+// JavaScript Conditions - If Else
+// Program 2
+// ========================================
+
+
+// ---------- If Else Statement ----------
+
+let marks = 65;
+
+if (marks >= 35) {
+
+    console.log("Pass");
+
+} else {
+
+    console.log("Fail");
+
+}
