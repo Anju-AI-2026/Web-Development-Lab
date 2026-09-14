b8d1136b5d60d2a8d3129e3b4e08f02f8c095eb2
Anjali Ramesh Sangannavar <sangannavaranjali@gmail.com>
2026-09-14 15:14:42 +0530

    Add the boolean.js file to 06_JavaScript folder which has the practice work on boolean data type

---
+++ 06_JavaScript/03_boolean.js
@@ -0,0 +1,24 @@
+// ========================================
+// JavaScript Basics - Boolean
+// Program 3
+// ========================================
+
+
+// ---------- Boolean Values ----------
+
+let hasShade = true;
+
+let isCrowded = false;
+
+let hasCrossing = true;
+
+console.log(hasShade);
+console.log(isCrowded);
+console.log(hasCrossing);
+
+
+// ---------- Boolean from Comparison ----------
+
+const age = 13;
+
+console.log(age >= 18);
