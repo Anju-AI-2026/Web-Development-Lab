2a62bb45917a6c24b12b0a5bb40203dbbfa0593b
Anjali Ramesh Sangannavar <sangannavaranjali@gmail.com>
2026-09-14 15:13:01 +0530

    Create a new folder called 06_JavaScript to add related pratice work on JS which is helpful for web development

---
+++ 06_JavaScript/01_variable.js
@@ -0,0 +1,35 @@
+// ========================================
+// JavaScript Basics - Variables & Numbers
+// File 1
+// ========================================
+
+// ---------- let variable and reassignment ----------
+
+let age = 18;
+
+console.log(age);
+
+age = 25;
+
+console.log(age);
+
+
+// ---------- const variable ----------
+
+const subjectName = "Python";
+
+console.log(subjectName);
+
+
+// ---------- Number data type ----------
+
+let new_age = 18;
+let price = 25.55;
+
+console.log(new_age);
+console.log(price);
+
+// Reassigning a let variable
+new_age = 25;
+
+console.log(new_age + price);
