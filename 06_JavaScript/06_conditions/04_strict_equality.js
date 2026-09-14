f180d3822d3bda427d80e5c53ab849c3096a507c
Anjali Ramesh Sangannavar <sangannavaranjali@gmail.com>
2026-09-14 15:24:34 +0530

    Add the \04_strict_equality.js file which has the practice work on strict equality in data type of js

---
+++ 06_JavaScript/06_conditions/04_strict_equality.js
@@ -0,0 +1,15 @@
+// ========================================
+// JavaScript Conditions - Strict Equality
+// Program 4
+// ========================================
+
+
+// ---------- Strict Equality (===) ----------
+
+let age = 18;
+
+if (age === 18) {
+
+    console.log("True");
+
+}
