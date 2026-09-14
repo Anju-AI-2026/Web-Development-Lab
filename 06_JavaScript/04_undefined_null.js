fc9991a17b9834737bcd8b2726ce8fad650d4010
Anjali Ramesh Sangannavar <sangannavaranjali@gmail.com>
2026-09-14 15:15:59 +0530

    Add the undefined_null.js file to 06_JavaScript folder which has the practiced work on undefined and null dataa type

---
+++ 06_JavaScript/04_undefined_null.js
@@ -0,0 +1,26 @@
+// ========================================
+// JavaScript Basics - Undefined & Null
+// Program 4
+// ========================================
+
+
+// ---------- Undefined ----------
+
+let subjectName;
+
+console.log(subjectName);
+
+subjectName = "Cloud Computing";
+
+console.log(subjectName);
+
+
+// ---------- Null ----------
+
+let finalMarks = null;
+
+console.log(finalMarks);
+
+finalMarks = 567;
+
+console.log(finalMarks);
