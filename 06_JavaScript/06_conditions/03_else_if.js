bef0ddeafc0885aa4a0a39145e7e19be3ab74cf7
Anjali Ramesh Sangannavar <sangannavaranjali@gmail.com>
2026-09-14 15:20:58 +0530

    Add the 03_else_if.js file to the 06_conditions folder which has the practiced work on if and else if statement

---
+++ 06_JavaScript/06_conditions/03_else_if.js
@@ -0,0 +1,23 @@
+// ========================================
+// JavaScript Conditions - Else If
+// Program 3
+// ========================================
+
+
+// ---------- Else If Statement ----------
+
+let marks = 25;
+
+if (marks >= 75) {
+
+    console.log("A+");
+
+} else if (marks >= 60 && marks <= 75) {
+
+    console.log("B+");
+
+} else if (marks <= 35) {
+
+    console.log("C+");
+
+}
