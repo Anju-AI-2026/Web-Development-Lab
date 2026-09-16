d9d386aebb8e12cf637c3b8b71d7e5f7b70cf781
Anjali Ramesh Sangannavar <sangannavaranjali@gmail.com>
2026-09-16 17:08:45 +0530

    add the and operator related practiced work to 06_conditions

---
+++ 06_JavaScript/06_conditions/05_and_operator.js
@@ -0,0 +1,72 @@
+// ========================================
+// JavaScript Conditions - AND Operator
+// Program 5
+// ========================================
+
+
+// ---------- AND Operator (&&) ----------
+
+let age = 20;
+
+let hasID = true;
+
+if (age == 20 && hasID == true) {
+
+    console.log("Allowed");
+
+} else {
+
+    console.log("Not allowed");
+
+}
+
+
+// ---------- Test 2 ----------
+
+age = 18;
+
+hasID = true;
+
+if (age == 20 && hasID == true) {
+
+    console.log("Allowed");
+
+} else {
+
+    console.log("Not allowed");
+
+}
+
+
+// ---------- Test 3 ----------
+
+age = 20;
+
+hasID = false;
+
+if (age == 20 && hasID == true) {
+
+    console.log("Allowed");
+
+} else {
+
+    console.log("Not allowed");
+
+}
+
+
+// ---------- Test 4 ----------
+
+age = 18;
+
+hasID = false;
+
+if (age == 20 && hasID == true) {
+
+    console.log("Allowed");
+
+} else {
+
+    console.log("Not allowed");
+
+}
