fb0ed0e41151579fdcf9e96fc0d36a34cb898abb
Anjali Ramesh Sangannavar <sangannavaranjali@gmail.com>
2026-09-16 17:13:23 +0530

    Add the not operator practiced related work to 07_not_operator.js file

---
+++ 06_JavaScript/06_conditions/07_not_operator.js
@@ -0,0 +1,15 @@
+// ========================================
+// JavaScript Conditions - NOT Operator
+// Program 7
+// ========================================
+
+
+// ---------- NOT Operator (!) ----------
+
+let isClosed = false;
+
+if (!isClosed) {
+
+    console.log("Open");
+
+}
