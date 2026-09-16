0570ee3f8b833ca61bd3abddbabdd255c36ff2ce
Anjali Ramesh Sangannavar <sangannavaranjali@gmail.com>
2026-09-16 17:19:57 +0530

    Add the for of loop practiced related work to 03_for_of_loop.js file

---
+++ 06_JavaScript/07_loops/03_for_of_loop.js
@@ -0,0 +1,15 @@
+// ========================================
+// JavaScript Loops - For Of Loop
+// Program 3
+// ========================================
+
+
+// ---------- For Of Loop ----------
+
+const languages = ["Python", "Java", "Javascript", "HTML"];
+
+for (const language of languages) {
+
+    console.log(language);
+
+}
