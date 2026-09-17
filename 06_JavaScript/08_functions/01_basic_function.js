d26bdb756a3b712e1326d02b437690c8863aa48b
Anjali Ramesh Sangannavar <sangannavaranjali@gmail.com>
2026-09-17 18:49:49 +0530

    Create a new folder called function to add function related program file there

---
+++ 06_JavaScript/08_functions/01_basic_function.js
@@ -0,0 +1,15 @@
+// ========================================
+// JavaScript Functions - Basic Function
+// Program 1
+// ========================================
+
+
+// ---------- Basic Function ----------
+
+function hello() {
+
+    console.log("Hello, Welcome to Javascript world.");
+
+}
+
+hello();
