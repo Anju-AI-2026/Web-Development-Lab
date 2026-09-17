4d1f7b166ee3bce7350381c322a4204e2b8f4c7b
Anjali Ramesh Sangannavar <sangannavaranjali@gmail.com>
2026-09-17 18:52:26 +0530

    Add the new file which containe arrow function related practice work

---
+++ 06_JavaScript/08_functions/04_arrow_function.js
@@ -0,0 +1,15 @@
+// ========================================
+// JavaScript Functions - Arrow Function
+// Program 4
+// ========================================
+
+
+// ---------- Arrow Function ----------
+
+const add = (a, b) => {
+
+    return a * b;
+
+};
+
+console.log(add(2, 3));
