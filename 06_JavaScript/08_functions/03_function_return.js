be1dd4cf6452a45d4627ea95e39148d35fc7762b
Anjali Ramesh Sangannavar <sangannavaranjali@gmail.com>
2026-09-17 18:51:46 +0530

    Add the new file which containe function return related practice work

---
+++ 06_JavaScript/08_functions/03_function_return.js
@@ -0,0 +1,15 @@
+// ========================================
+// JavaScript Functions - Return
+// Program 3
+// ========================================
+
+
+// ---------- Function with Return ----------
+
+function add(a, b) {
+
+    return a + b;
+
+}
+
+console.log(add(5, 10));
