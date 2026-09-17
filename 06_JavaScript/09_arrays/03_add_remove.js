a74c283890bf15f21e627b2091be6843bb774f1a
Anjali Ramesh Sangannavar <sangannavaranjali@gmail.com>
2026-09-17 18:55:29 +0530

    Add the add and removing the array element array work related code file to array folder

---
+++ 06_JavaScript/09_arrays/03_add_remove.js
@@ -0,0 +1,22 @@
+// Original Array
+const activity = ["Cook", "Read", "Dance", "Study"];
+
+console.log(activity);
+
+// Push element
+activity.push("Drive");
+
+console.log(activity);
+
+// Pop Element
+activity.pop();
+
+console.log(activity);
+
+// Unshift 
+activity.unshift("Write");
+console.log(activity);
+
+// Shift
+activity.shift();
+console.log(activity);
