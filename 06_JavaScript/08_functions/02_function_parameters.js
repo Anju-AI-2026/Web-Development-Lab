811fa064aeea372e90de7f9f7800ee46d911e83b
Anjali Ramesh Sangannavar <sangannavaranjali@gmail.com>
2026-09-17 18:50:58 +0530

    Add the new file which containe function parameters related practice work

---
+++ 06_JavaScript/08_functions/02_function_parameters.js
@@ -0,0 +1,15 @@
+// ========================================
+// JavaScript Functions - Parameters
+// Program 2
+// ========================================
+
+
+// ---------- Function with Parameter ----------
+
+function hello(name) {
+
+    console.log(`Hello ${name} Good evening`);
+
+}
+
+hello("Anjali");
