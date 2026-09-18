ce31ca4f7a9a6c7ddc83951ffb2498fa2e786554
Anjali Ramesh Sangannavar <sangannavaranjali@gmail.com>
2026-09-18 18:18:43 +0530

    Create a new folder called Objects and add the object methods  related praticed things

---
+++ 06_JavaScript/10_objects/04_object_methods.js
@@ -0,0 +1,11 @@
+const student = {
+    name: "Riya",
+    age: 20,
+    language: "JavaScript"
+};
+
+console.log(student);
+
+console.log(Object.keys(student));
+console.log(Object.values(student));
+console.log(Object.entries(student));
