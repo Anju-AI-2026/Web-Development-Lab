9993c58a41ac64d1fb5d67341b1081ec9f016824
Anjali Ramesh Sangannavar <sangannavaranjali@gmail.com>
2026-09-18 18:13:57 +0530

    Create a new folder called Objects and add the modify add related praticed things

---
+++ 06_JavaScript/10_objects/02_modify_add.js
@@ -0,0 +1,15 @@
+const employee = {
+    name: "Arjun",
+    age: 28,
+    salary: "20,000"
+};
+
+console.log(employee);
+
+employee.age = 30;
+
+console.log(employee);
+
+employee.city = "Hubli";
+
+console.log(employee);
