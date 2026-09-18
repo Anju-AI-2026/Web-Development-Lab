f077da00905dbe923fbbf20fa46d1c17bb9e1b76
Anjali Ramesh Sangannavar <sangannavaranjali@gmail.com>
2026-09-18 18:12:53 +0530

    Create a new folder called Objects and add the object related praticed things

---
+++ 06_JavaScript/10_objects/01_create_access.js
@@ -0,0 +1,10 @@
+const student = {
+    name: "Riya",
+    age: 20,
+    language: "JavaScript"
+};
+
+console.log(student);
+console.log(student.name);
+console.log(student.language);
+console.log(student["age"]);
