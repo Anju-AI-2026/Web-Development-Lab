c836e2dd66697072a41a118904ecd6f26b91ab5f
Anjali Ramesh Sangannavar <sangannavaranjali@gmail.com>
2026-09-19 15:14:24 +0530

    Add the new script file to the classList folder

---
+++ 06_JavaScript/13_DOM/03_classList/script.js
@@ -0,0 +1,9 @@
+const heading = document.getElementById("title");
+
+heading.classList.add("highlight");
+
+heading.classList.remove("highlight");
+
+heading.classList.toggle("highlight");
+
+heading.classList.contains("highlight");
