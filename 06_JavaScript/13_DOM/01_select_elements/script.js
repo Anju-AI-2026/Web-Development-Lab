ea30e0fc8dcabb5f06261264a0011ab0130ee9e5
Anjali Ramesh Sangannavar <sangannavaranjali@gmail.com>
2026-09-19 15:09:54 +0530

    Add the script of js file to select element which helps for the html file

---
+++ 06_JavaScript/13_DOM/01_select_elements/script.js
@@ -0,0 +1,13 @@
+const heading = document.getElementById("title");
+
+console.log(heading);
+
+heading.textContent = "Hello Anjali";
+
+const message = document.querySelector(".message");
+
+document.querySelector("#title");
+
+const items = document.querySelectorAll(".item");
+
+console.log(items);
