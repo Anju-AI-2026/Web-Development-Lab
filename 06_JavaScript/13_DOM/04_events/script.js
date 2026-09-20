f6bb890183f221c6a62b4d82d8561cea791e4d79
Anjali Ramesh Sangannavar <sangannavaranjali@gmail.com>
2026-09-20 18:44:52 +0530

    Cfreate the js file which have the code of action required in html file

---
+++ 06_JavaScript/13_DOM/04_events/script.js
@@ -0,0 +1,10 @@
+
+const heading = document.getElementById("title");
+
+const button = document.getElementById("btn");
+
+button.addEventListener("click", function () {
+
+    console.log("Button clicked!");
+    
+});
